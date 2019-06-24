98ec80c2aacf5a2c7221d8e16787378a8d4843de
AlexisLELEU <alexis.leleu@hetic.net>
2019-06-24 21:31:02 +0200

    Bliink test react redux

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# React-Redux-jsonPlaceholder
+### ReactRedux-Starter ###
+
+Pour installer le projet se placer dans le projet et lancer : 
+
+```
+> npm install
+> npm start
+```
+
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { store } from './configureStore'
+import App from './components/app'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'))
